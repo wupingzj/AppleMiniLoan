d77744db180ec74f55d23e026476e48b06a2eb62
Kevin Wu <kevinpingwu2@gmail.com>
2014-03-14 07:48:51 +1100

    Initial Commit

---
+++ MiniLoan/YANGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YANGAppDelegate.h
+//  MiniLoan
+//
+//  Created by Ping on 14/03/14.
+//  Copyright (c) 2014 Yang Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YANGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniLoanTests/MiniLoanTests.m
@@ -0,0 +1,34 @@
+//
+//  MiniLoanTests.m
+//  MiniLoanTests
+//
+//  Created by Ping on 14/03/14.
+//  Copyright (c) 2014 Yang Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiniLoanTests : XCTestCase
+
+@end
+
+@implementation MiniLoanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
