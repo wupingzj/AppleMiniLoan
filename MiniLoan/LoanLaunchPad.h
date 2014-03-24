44a04d76facd2c317782bf079507f4e921c9ad78
Kevin Wu <kevinpingwu2@gmail.com>
2014-03-24 20:06:00 +1100

    Change the view controllers to LoanLaunchPad and Offer classes.

---
+++ MiniLoan/LoanLaunchPad.h
@@ -0,0 +1,13 @@
+//
+//  LoanLaunchPad.h
+//  MiniLoan
+//
+//  Created by Ping on 24/03/14.
+//  Copyright (c) 2014 Yang Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoanLaunchPad : UIViewController
+
+@end
